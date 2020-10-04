01731f0cf2e5d08758ce526a74ea52caa67c5289
Man Ioan <man_ioan@live.com>
2020-10-04 16:57:31 +0300

    Add customer component to application

---
+++ Northwind.Web/ClientApp/src/app/customer/customer.component.ts
@@ -0,0 +1,95 @@
+
+    import { Component } from '@angular/core';  
+    import { CustomerService } from 'src/services/CustomerService';  
+    import { FormGroup, FormControl,Validators } from '@angular/forms';   
+    @Component({  
+      selector: 'app-customer-component',  
+      templateUrl: './customer.component.html',  
+      styleUrls: ['./customer.component.css']  
+    })  
+    export class CustomerComponent {  
+      title = 'CustomerComponent';  
+         
+      constructor(private CustomerService: CustomerService) { }  
+      data: any;  
+      CstForm: FormGroup;  
+      submitted = false;   
+      EventValue: any = "Save";  
+      
+      ngOnInit(): void {  
+        this.getdata();  
+      
+        this.CstForm = new FormGroup({  
+        CustomerID: new FormControl(null),  
+        CompanyName: new FormControl("",[Validators.required]),        
+        ContactName: new FormControl("",[Validators.required]),  
+        ContactTitle:new FormControl("",[Validators.required]),
+          Address:new FormControl("",[Validators.required]),
+        ContactTitle:new FormControl("",[Validators.required]),
+        ContactTitle:new FormControl("",[Validators.required]),
+        ContactTitle:new FormControl("",[Validators.required]),
+        ContactTitle:new FormControl("",[Validators.required]),
+        ContactTitle:new FormControl("",[Validators.required]),
+        ContactTitle:new FormControl("",[Validators.required]),
+  
+        })    
+      }  
+      getdata() {  
+        this.CustomerService.getData().subscribe((data: any[]) => {  
+          this.data = data;  
+        })  
+      }  
+      deleteData(id) {  
+        this.CustomerService.deleteData(id).subscribe((data: any[]) => {  
+          this.data = data;  
+          this.getdata();  
+        })  
+      }  
+      Save() {   
+        this.submitted = true;  
+        
+         if (this.CstForm.invalid) {  
+                return;  
+         }  
+        this.CustomerService.postData(this.CstForm.value).subscribe((data: any[]) => {  
+          this.data = data;  
+          this.resetFrom();  
+      
+        })  
+      }  
+      Update() {   
+        this.submitted = true;  
+        
+        if (this.CstForm.invalid) {  
+         return;  
+        }        
+        this.CustomerService.putData(this.CstForm.value.CustomerID,this.CstForm.value).subscribe((data: any[]) => {  
+          this.data = data;  
+          this.resetFrom();  
+        })  
+      }  
+      
+      EditData(Data) {  
+        this.CstForm.controls["CustomerID"].setValue(Data.CustomerID);  
+        this.CstForm.controls["CompanyName"].setValue(Data.CompanyName);      
+        this.CstForm.controls["ContactName"].setValue(Data.ContactName);  
+        this.CstForm.controls["ContactTitle"].setValue(Data.ContactTitle);  
+        this.CstForm.controls["Address"].setValue(Data.Address); 
+        this.CstForm.controls["City"].setValue(Data.City); 
+        this.CstForm.controls["Region"].setValue(Data.Region); 
+        this.CstForm.controls["PostalCode"].setValue(Data.PostalCode); 
+        this.CstForm.controls["Country"].setValue(Data.Country); 
+        this.CstForm.controls["Phone"].setValue(Data.Phone); 
+        this.CstForm.controls["Fax"].setValue(Data.Fax); 
+        this.EventValue = "Update";  
+      }  
+      
+      resetFrom()  
+      {     
+        this.getdata();  
+        this.CstForm.reset();  
+        this.EventValue = "Save";  
+        this.submitted = false;   
+      }  
+    }  
+
