fa0df2496d2dee4f5156042eae6f5cc470eca88b
Man Ioan <man_ioan@live.com>
2020-10-21 18:49:36 +0300

    Implement API for Supplier entity

---
+++ Northwind.Api/Controllers/SupplierController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Api.Models;
+
+namespace Northwind.Api.Controllers
+{
+    [Route("api/Supplier")]
+    [ApiController]
+    public class SupplierController : ControllerBase
+    {
+        readonly NorthwindContext _context;
+        public SupplierController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Supplier> Get()
+        {
+            var data = _context.Suppliers.ToList();
+            return data;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Supplier obj)
+        {
+            var data = _context.Suppliers.Add(obj);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Supplier obj)
+        {
+            var data = _context.Suppliers.Update(obj);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var data = _context.Suppliers.Where(a => a.SupplierID == id).FirstOrDefault();
+            _context.Suppliers.Remove(data);
+            _context.SaveChanges();
+            return Ok();
+
+        }
+    }
+}
